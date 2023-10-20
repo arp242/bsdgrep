0c7e17966038dadada838356d884c2979a20edcc
Martin Tournoij <martin@arp242.net>
2023-10-20 20:58:46 +0100

    These constants are no longer used

--- freebsd.h
+++ freebsd.h
@@ -3,20 +3,9 @@
 
 const char * getprogname(void);
 
-// https://github.com/freebsd/freebsd-src/blob/b9f04b3/sys/sys/cdefs.h#L581
-// Empty anyway, so no need to embed it.
-#ifndef	__FBSDID
-#define	__FBSDID(s)	struct __hack
-#endif
-
 #ifndef __DECONST
 #include <stdint.h>
 #define __DECONST(type, var)    ((type)(uintptr_t)(const void *)(var))
-#endif
-
-// https://github.com/freebsd/freebsd-src/blob/b500c184/tools/build/cross-build/include/linux/limits.h
-#ifndef OFF_MAX
-#define OFF_MAX UINT64_MAX
 #endif
 
 // https://github.com/freebsd/freebsd-src/blob/098dbd7/lib/libc/sys/mmap.2#L233
